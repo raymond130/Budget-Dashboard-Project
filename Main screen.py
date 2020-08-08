4267966ab07fa8d911f960b657b41464ec8a3ef7
raymond130 <45745238+raymond130@users.noreply.github.com>
2020-08-08 17:10:58 -0400

    Add files via upload

---
+++ Main screen.py
@@ -0,0 +1,79 @@
+from tkinter import *
+from ledger import *
+from PIL import ImageTk, Image
+import datetime
+import tkinter.font as tkfont
+
+
+# def callback():
+#   print ("called the callback!")
+
+# def manage_billspay():
+#    print ("")
+
+# def input_expense():
+#   print ("")
+
+# def modify_expenses():
+#   print ("")
+
+
+# def input_income():
+#   print ("")
+
+def greenRed(amount):
+    # add gradient for amount over 0 later
+    redGreen = amount > 0
+    if redGreen:
+        greenRed = " #95f134 "
+    else:
+        greenRed = " #f3612a "
+    return redGreen
+
+
+# get ledger file from user input
+# open ledger file and load ledger
+
+mainLedger = Account()
+
+print(mainLedger.total_amount)
+
+root = Tk()
+
+# displays the title image
+path = (r"C:\Users\Brandon\PycharmProjects\DesktopBudget\venv\Lib\site-packages\desktop budget tool logo finished.png")
+img = ImageTk.PhotoImage(Image.open(path))
+titleFrame = Label(root, image=img).grid(row=0, column=0)
+
+# displays the summary frame
+summaryBckd = greenRed(str(mainLedger.total_amount))
+budgetSummaryFrame = Frame(root, background=summaryBckd, borderwidth=1).grid(row=1, column=0)
+
+remainingBalance = Label(budgetSummaryFrame \
+                         , text="Remaining free cash = " + mainLedger.totalAmount \
+                         , bg=greenRed(mainLedger.totalAmount)
+                         , font=tkfont.font(family="Helvetiva", size=36, weight=" normal", slant=" roman"))
+
+# displays the top of the ledger in a labelframe
+# upcoming periodic expenses
+upcomingExpenses = LabelFrame(root, text="Upcoming Expenses").grid(row=2, column=0)
+# insert label(grid) here?
+# grid pulls the next five periodic expenses
+periodicEntriesHead = head()
+n = 5
+for entry in mainLedger.periodicEntries:
+    if entry["date"].day > datetime.now().day:
+        periodicEntriesHead.update(entry)
+# index entries in periodicEntriesHead to display in order of date.
+for entry in periodicEntriesHead:
+    if (i > 0):
+        label(upcomingExpenses, text=entry["name"]).grid(row=0, sticky=W)
+        label(upcomingExpenses, text=entry["amount"]).grid(row=0, column=1)
+        label(upcomingExpenses, text=entry["date"]).grid(row=0, column=2)
+        i = i - 1
+
+# displays the most recent expense items
+# most recent expenses
+
+
+root.mainloop()
---
+++ ledger.py
@@ -0,0 +1,119 @@
+# main accounting ledger for the budget tool
+# keeps item name, description, category,  debit/credit,  notes, entry date, and last entry date
+# works  with the file ledger.txt
+
+import json
+import os
+
+
+# creates ledger_entry class with a contsructor that includes the relevant information
+# ledger_output will be a dictonary with the relevant information
+# another ledger constructor (for reading from the JSON) will take a dictionary and construct an object
+
+# reads current ledger first, then allows modification and creation
+
+# todo: open and read in ledger entries on file initialization - only reads in the JSON dict
+#   must create a backup of the ledger first
+# modifying ledger entries by reading an entry from the dictionary returned by the json read and overwriting the dict entries
+#   first creates a new ledger entry with the information from the json dump
+#   modifies the information as necessary
+#   outputs the data as a dict, then overwriters the relevant ledger entry
+#   refers to ledger entries by number
+# outputting to the ledger in a json format
+
+class Account:
+    def __init___(self, json_dict=None, ledger=[], name="", total_amount=0, periodicEntries=[]):
+        if json_dict is not None:
+            self.ledger = json_dict['ledger']
+            self.name = json_dict['name']
+            self.total_amount = json_dict['total_amount']
+            self.periodic_entries = json_dict['periodic_entries']
+        else:
+            self.ledger = []
+            self.name = ""
+            self.total_amount = 0
+            self.periodic_entries = []
+
+        print("test")
+        print (str(self.total_amount))
+
+class ledger_list:
+
+    def __init__(self,json = None, entries = None):
+        if (json is not None):
+            self.entries = jason['entries']
+
+
+class ledger_entry:
+    def __init__(self, ledger, json=None, number=0, name=None, description=None \
+                 , category=None, debit_credit=None, notes=None, entry_date=None, last_update_date=None):
+        if (json is not None):
+            self.ledger = json['ledger']
+            self.number = json["number"]
+            self.name = json["name"]
+            self.description = json["description"]
+            self.category = json["category"]
+            self.debit_credit = json["debit_credit"]
+            self.notes = json["notes"]
+            self.entry_date = json["entry_date"]
+            self.last_update_date = json["last_update_date"]
+
+        else:
+            self.ledger = ledger
+            self.number = number
+            self.name = name
+            self.description = description
+            self.category = category
+            self.debit_credit = debit_credit
+            self.notes = notes
+            self.date = entry_date
+            self.update_date = last_update_date
+
+    def printOut(self):
+        printed = {
+            "ledger": self.leger,
+            "number": self.number,
+            "name": self.name,
+            "description": self.description,
+            "category": self.category,
+            "debit_credit": self.debit_credit,
+            "notes": self.notes,
+            "entry_date": self.entry_date,
+            "last_update_date": self.last_update_date
+        }
+
+        return printed
+
+
+def new_ledger_entry(account, entry):
+    # adds a new ledger entry, sets the number of the ledger entry after the number of the last entry in ledger
+    num = str(len(account.ledger))
+    account.ledger.update(num, entry)
+
+
+def cash_Update(account, amount):
+    account.total_amount = amount
+
+
+def add_periodical(ledger, entry):
+    ledger['periodic entries'].update(str(len(ledger['periodic entries'])), entry)
+
+
+ledger = {}
+ledger['entries'] = []
+ledger["index"] = 0
+ledger['remaining budget'] = 0
+ledger['periodic entries'] = []
+
+
+
+with open('fileledger.txt', 'w+') as ledgerFile:
+    if os.stat('fileledger.txt').st_size > 0:
+        data = json.load(ledgerFile)
+    else:
+        data = ''
+ledger = data
+
+with open('ledger.txt.', 'w') as fileout:
+    storedData = json.dumps((ledger))
+    json.dump(storedData, fileout)
